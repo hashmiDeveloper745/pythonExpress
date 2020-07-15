b5e306ed3409568622f2d59cd93d03b5ef1dda7b
developerfaraz745 <muhammadfaraz745@gmail.com>
2020-07-15 19:15:16 +0500

    Python, MySql CURD Complete.

--- pythonDB.py
+++ pythonDB.py
@@ -61,4 +61,40 @@ def showTables(dbs):
       print("Database: "+str(dbs)+";")
   except:
     print("Error: "+str(sys.exc_info()[1]))
-  
+def insertData(employeeName):
+  try:
+    getDatabaseConnection = connectDatabase()
+    if getDatabaseConnection:
+      getCursor = getDatabaseConnection.cursor()
+      query = "insert into employees (empName) values (%s)"
+      valus = [employeeName]
+      getCursor.execute(query, valus)
+      getDatabaseConnection.commit()
+      print(str(getCursor.rowcount)+" rows inserted")
+  except:
+    print("Error occur: "+str(sys.exc_info()[1]))
+def updataRecord(emplId, emplName):
+  try:
+    getDatabaseConnection = connectDatabase()
+    if getDatabaseConnection:
+      getCursor = getDatabaseConnection.cursor()
+      query = "update employees set empName = %s where empId = %s;"
+      values = [emplName, emplId]
+      getCursor.execute(query,values)
+      getDatabaseConnection.commit()
+      print(str(getCursor.rowcount)+" row(s) updated")
+  except:
+    print("Error occur: "+str(sys.exc_info()[1]))
+def removeRecord(emplId):
+  try:
+    getDatabaseConnection = connectDatabase()
+    if getDatabaseConnection:
+      getCursor = getDatabaseConnection.cursor()
+      query = "Delete from employees where empId = %s;"
+      values = [emplId]
+      getCursor.execute(query,values)
+      getDatabaseConnection.commit()
+      print(str(getCursor.rowcount)+" row(s) updated")
+  except:
+    print("Error occur: "+str(sys.exc_info()[1]))
+    
--- pythonDBInsert.py
+++ pythonDBInsert.py
@@ -1,21 +1,10 @@
-import mysql.connector
-import sys
-""" 
-Insert Example
-"""
+import sys, mysql.connector
+
 getH = "localhost"
 getU = "developerfarazhashmi"
 getP = "Jk*%43qAsXtU"
 getDB = "pythonExpress"
-try:
-	connectDB = mysql.connector.connect(host = getH, username = getU, password = getP, database = getDB);
-	if connectDB:
-		writingCursor = connectDB.cursor()
-		query = "show tables;"
-		writingCursor.execute(query)
-		getTables = writingCursor.fetchall()
-		for tables in getTables:
-			print(tables)
-except:
-	errorStatement = "Error Occur: "+str(sys.exc_info()[1])+"."
-	print(errorStatement)
+
+class pythonDBInsert:
+
+pDBI = pythonDBInsert()
