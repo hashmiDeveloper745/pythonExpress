4579851cdc7a09384ab389ae03f1e522ad069b9f
developerfaraz745 <muhammadfaraz745@gmail.com>
2020-07-17 16:10:10 +0500

    Lesson Progress

--- example.py
+++ example.py
@@ -1,11 +1,19 @@
-def tri_recursion(k):
-  if(k>0):
-    result = k+tri_recursion(k-1)
-    print(result)
-  else:
-    result = 0
-  return result
+import functions, sys
+class example:
+	def __init__(self):
+		print("Python Example")
+	def tri_recursion(self, k):
+		if(k>0):
+			result = (k+int((self.tri_recursion(k-1))))
+			print(result)
+		else:
+			result = 0
+			return result
+		#print("\n\nRecursion Example Results")
+		#tri_recursion(6)
 
-print("\n\nRecursion Example Results")
-tri_recursion(6)
-
+try:
+	e = example()
+	e.tri_recursion(94)
+except:
+	print("Error Occur: "+str(sys.exc_info()[1])+".")
--- functions.py
+++ functions.py
@@ -5,34 +5,41 @@ import platform
 import os
 class functions:
   dicts = {1,2,4,5,6,7,8,9,0}
-  def __init__():
-    print("Hello")
-def userInput():
-  someText = str(input("Enter any text string: "))
-  return someText
-def todayTheDateIs():
-  getDate = (datetime.datetime(2019,2,14))
-  makeText = (str(platform.system())+": "+str(getDate.strftime("%A, %B - %d - %Y")))
-  return makeText
-def getTheYear():
-  getDate = datetime.datetime(2019,1,1)
-  fetchYear = getDate
-  return (fetchYear)
-def letsRead(fileName):
-  getFile = str(fileName)
-  q = open(getFile,"rt")
-  print(q.read())
-def letsWrite(fieName):
-  getFile = str(fieName)
-  p = open(getFile,"w")
-def letsUpdate(fileName):
-  getFile = str(fileName)
-  y = open(getFile,"a")
-def letsCreateFile(fileName, exte):
-  getFile = str(fileName)
-  getExtension = str(exte)
-  n = open(getFile+getExtension,"x")
-def removeIt(fileName):
-  getFile = str(fileName)
-  os.path.exists(getFile)
-
+  def __init__(self):
+    print(self)
+  def userInput(self, text):
+    someText = str(input(text))
+    return someText
+  def todayTheDateIs(self):
+    getDate = (datetime.datetime(2019,2,14))
+    makeText = (str(platform.system())+": "+str(getDate.strftime("%A, %B - %d - %Y")))
+    return makeText
+  def getTheYear(self):
+    getDate = datetime.datetime(2019,1,1)
+    fetchYear = getDate
+    return (fetchYear)
+  def letsRead(self, fileName):
+    getFile = str(fileName)
+    q = open(getFile,"rt")
+    print(q.read())
+  def letsWrite(self, fieName):
+    getFile = str(fieName)
+    p = open(getFile,"w")
+  def letsUpdate(self, fileName):
+    getFile = str(fileName)
+    y = open(getFile,"a")
+  def letsCreateFile(self, fileName, exte):
+    getFile = str(fileName)
+    getExtension = str(exte)
+    n = open(getFile+getExtension,"x")
+  def removeIt(self,fileName):
+    getFile = str(fileName)
+    os.path.exists(getFile)
+  def celsiusToFahrenheit(self):
+    temperatureInCelsius = float(userInput("Enter Temperature in Celcius: "))
+    ToFahrenheit = ((temperatureInCelsius * (9/5)) + 32)
+    print("Temperature in Fahrenheit is "+str(ToFahrenheit))
+  def fahrenheitToCelsius(self):
+    temperatureInFahrenheit = float(self.userInput("Enter Temperature in fahrenheit: "))
+    ToCelsius = (((temperatureInFahrenheit - 32 ) * (9/5)))
+    print("Temperature in Celsius is "+str(ToCelsius))
